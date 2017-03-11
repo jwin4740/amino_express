34c14ddc0e7b0864edd58f578ba27397aff9d667
James <jwin4740@gmail.com>
2017-03-10 23:06:51 -0500

    nice images

--- routes/routeIndex.js
+++ routes/routeIndex.js
@@ -1,18 +1,32 @@
 var aminoArray = require('../data/aminoacids.js');
-console.log(aminoArray[0]);
+
 exports.home = function (request, response) {
 	response.render("home", {
 		tagline : "Let's build a protein"
 	});
 };
 
-aminoArray[0].name = "alanine"
+var index;
+
 // allows user to type in a number in the url (which is optional because question mark)
 	// the user input is stored in the episodeNumber variable
 exports.amino_acid = function (request, response) {
 	var aminoAcid = request.params.amino_acid;
+	var n = aminoArray.length;
+for (var i = 0; i < n; i++){
+	if (aminoAcid === aminoArray[i].name)
+	{
+		index = i;
+	}
+	else {
+		console.log("missed");
+	}
+}
+console.log(index);
 	response.render("aminopages", {
-		output : aminoAcid
+		name : aminoArray[index].name,
+		stick : aminoArray[index].ballStickImage,
+		skeleton: aminoArray[index].skeletalImage
 	});
 };
 
